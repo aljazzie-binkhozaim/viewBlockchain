9e8648fc13da9e496d61ab323a40739b1f941728
aljazzie bin khozaim <84498714+aljazzie-binkhozaim@users.noreply.github.com>
2023-12-07 06:34:35 +0300

    Update Feed.tsx trying to add search bar

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -12,6 +12,7 @@ interface Student {
   studentMarks: string;
   username: string;
 }
+
 interface Props {
   provider: any
 }
@@ -20,23 +21,20 @@ const Feed: React.FC<Props> = ({ provider }) => {
   const [posts, setPosts] = useState<Student[]>([]);
   const [item, setItem] = useState({ name: "all" });
   const [active, setActive] = useState(0);
-
-
+  const [searchTerm, setSearchTerm] = useState('');
 
   const getUpdatedStudents = (allStudents: Student[], _address: string) => {
     let updatedStudent: Student[] = [];
     for (let i = 0; i < allStudents.length; i++) {
-    {
-        let student: Student = {
-          id: allStudents[i].id,
-          examName: allStudents[i].examName,
-          studentName: allStudents[i].studentName,
-          studentEmail: allStudents[i].studentEmail,
-          studentMarks: allStudents[i].studentMarks,
-          username: allStudents[i].username,
-        };
-        updatedStudent.push(student);
-      } 
+      let student: Student = {
+        id: allStudents[i].id,
+        examName: allStudents[i].examName,
+        studentName: allStudents[i].studentName,
+        studentEmail: allStudents[i].studentEmail,
+        studentMarks: allStudents[i].studentMarks,
+        username: allStudents[i].username,
+      };
+      updatedStudent.push(student);
     }
     return updatedStudent;
   }
@@ -91,57 +89,67 @@ const Feed: React.FC<Props> = ({ provider }) => {
   useEffect(() => {
     if (item.name === "all") {
       getAllStudents();
-      console.log(posts[0]);
-      
     } else {
       getCustomStudents();
     }
-    
-    // When the item changes, the useEffect is triggered
   }, [item]);
 
-  const handleClick = (e: React.MouseEvent<HTMLSpanElement, globalThis.MouseEvent>, index: number) => {
-    setItem({ name: e.currentTarget.textContent || '' });
-    setActive(index);
+  const handleSearch = () => {
+    const filteredStudents = posts.filter((post) =>
+      post.studentEmail.includes(searchTerm)
+    );
+    setPosts(filteredStudents);
   };
 
   return (
     <div>
-      <div >
-        {typeExam.map((item, index) => {
-          return (
-                  <span
-                    onClick={(e) => {
-                      handleClick(e, index);
-                    }}
-                    className={`${active === index ? 'active-work':''} work__item`}
-                    key={index}
-                  >
-                    {item.name}
-                  </span>
-                );
-              })}
-            </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by Student ID"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div>
+        {typeExam.map((item, index) => (
+          <span
+            onClick={(e) => {
+              setItem({ name: e.currentTarget.textContent || '' });
+              setActive(index);
+            }}
+            className={`${active === index ? 'active-work' : ''} work__item`}
+            key={index}
+          >
+            {item.name}
+          </span>
+        ))}
+      </div>
 
-            <ul className="cards">
-              {posts
-            .sort((a, b) => parseInt(String(b.id)) - parseInt(String(a.id))) // Sort in descending order based on id
-            .map((post) => (
-        <li className="cards_item">
-          <div className="card">
-            <div className="card_content" key={post.id}>
-              <p className="card_title">{parseInt(String(post.id))}</p>
-              <p className="card_title">{post.examName}</p>
-{/*               <p className="card_text"><b>Name:<br/> </b>{post.studentName}</p> */}
-              <p className="card_text"><b>StudentID:<br/> </b>{post.studentEmail.slice(0, post.studentEmail.indexOf('@'))}</p>
-              <p className="card_text"><b>Marks:<br/> </b>{post.studentMarks}</p>
-            </div>
-          </div>
-        </li>
-      ))}
+      <ul className="cards">
+        {posts
+          .sort((a, b) => parseInt(String(b.id)) - parseInt(String(a.id)))
+          .map((post) => (
+            <li className="cards_item" key={post.id}>
+              <div className="card">
+                <div className="card_content">
+                  <p className="card_title">{parseInt(String(post.id))}</p>
+                  <p className="card_title">{post.examName}</p>
+                  <p className="card_text">
+                    <b>Name:<br /> </b>{post.studentName}
+                  </p>
+                  <p className="card_text">
+                    <b>StudentID:<br /> </b>{post.studentEmail.slice(0, post.studentEmail.indexOf('@'))}
+                  </p>
+                  <p className="card_text">
+                    <b>Marks:<br /> </b>{post.studentMarks}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
       </ul>
-
-      
     </div>
   );
 }
